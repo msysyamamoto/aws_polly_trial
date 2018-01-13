bd759e17e644acc5d247f0baa42428b79b706a05
Masayasu Yamamoto <m2yamamoto@gmail.com>
2018-01-13 19:52:30 +0900

    Add IP Allow list

--- terraform/environments/variable.tf
+++ terraform/environments/variable.tf
@@ -1,3 +1,19 @@
 variable "region" {
   default = "us-west-2"
 }
+
+variable "whitelisted_ips" {
+  type = "list"
+
+  # IP Allow list
+  default = [
+    {
+      value = "2001:db8::/32"
+      type  = "IPV6"
+    },
+    {
+      value = "192.0.2.0/24"
+      type  = "IPV4"
+    }
+  ]
+}
