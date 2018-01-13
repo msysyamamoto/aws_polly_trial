96c0c0bce3f2918b87de7a3b4a0bdad988e1d32d
Masayasu Yamamoto <m2yamamoto@gmail.com>
2018-01-13 17:13:19 +0900

    Add lambda function

---
+++ terraform/modules/compute/lambda_function/lambda_handler.py
@@ -0,0 +1,37 @@
+import base64
+from itertools import groupby
+from operator import itemgetter
+from boto3 import Session
+
+def text_to_mp3(event, context):
+    session = Session()
+    polly = session.client("polly")
+
+    response = polly.synthesize_speech(Text=event['text'], OutputFormat="mp3", oiceId=event['voice'])
+
+    encoded = base64.b64encode(response['AudioStream'].read()).decode('utf-8')
+    
+    return encoded
+
+
+def describe_voices(event, context):
+    
+    session = Session()
+    polly = session.client("polly")
+
+    response = polly.describe_voices()
+
+    vs = response["Voices"]
+    langage_code_getter = itemgetter("LanguageCode")
+    sorted_voices       = sorted(vs, key=langage_code_getter)
+    grouping_voices     = groupby(vs, key=langage_code_getter)
+
+    res = {}
+    for (language_code, voices) in grouping_voices:
+        items = []
+        for v in voices:
+            items.append({"Gender": v["Gender"], "Id": v["Id"]})
+            
+        res[language_code] = items
+    
+    return res
